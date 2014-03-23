d01737b4f03c12b7d1ba4d580bf0dfd52eec978b
Rasel Ahmmed Bappi <raselahmmed@hotmail.com>
2014-03-23 11:03:31 +0600

    Update KendoUiHelper.cs

--- RnD.KendoUISample/RnD.KendoUISample/Helpers/KendoUiHelper.cs
+++ RnD.KendoUISample/RnD.KendoUISample/Helpers/KendoUiHelper.cs
@@ -14,56 +14,53 @@ namespace RnD.KendoUISample.Helpers
 
         private static KendoGridResult<T> ReturnGridData<T>(KendoGridPost requestParams, ref IQueryable<T> collection)
         {
+            List<T> gridData = new List<T>();
 
-            //If the sort Order is provided perform a sort on the specified column
-            if (requestParams.SortOrd.IsNotEmpty())
-            {
-                //Test
-                var qqq = collection.ToList();
-
-                var ddd = collection.OrderByDescending(x => x.GetType().GetProperty(requestParams.SortOn));
-                var aaa = collection.OrderBy(x => x.GetType().GetProperty(requestParams.SortOn));
-
-                var ddd1 = qqq.OrderByDescending(x => x.GetType().GetProperty(requestParams.SortOn));
-                var aaa1 = qqq.OrderBy(x => x.GetType().GetProperty(requestParams.SortOn));
-                //Test
-                
-                collection = requestParams.SortOrd == "desc"
-                             ? collection.OrderByDescending(x => requestParams.SortOn)
-                             : collection.OrderBy(x => requestParams.SortOn);
-            }
-
-            List<T> gridData;
             try
             {
-                gridData = collection.Skip(requestParams.Skip).Take(requestParams.PageSize).ToList();
+                //If the sort Order is provided perform a sort on the specified column
+                if (requestParams.SortOrd.IsNotEmpty())
+                {
+                    var sortCollection = Sort<T>(collection, requestParams.SortOn, requestParams.SortOrd);
+
+                    //If sort and paging
+                    gridData = sortCollection.Skip(requestParams.Skip).Take(requestParams.PageSize).ToList();
+                }
+                else
+                {
+                    //If only paging
+                    gridData = collection.Skip(requestParams.Skip).Take(requestParams.PageSize).ToList();
+                }
             }
             catch
             {
-                //collection = collection.OrderBy("id");
-                gridData = collection.Skip(requestParams.Skip).Take(requestParams.PageSize).ToList();
-            }
 
-            //var collectionType = typeof(T);
-            //var collectionProperties = collectionType.GetProperties();
-            //gridData.ForEach(row => {
-            //    foreach (var cProp in collectionProperties)
-            //    {
-            //        var pVal = cProp.GetValue(row, null);
-            //        if (pVal == null)
-            //        {
-            //            cProp.SetValue(row, "", null);
-            //        }
-            //    }
-            //});
+            }
 
             return new KendoGridResult<T>
             {
-                Data = gridData,
+                Data = gridData.Count > 0 ? gridData : collection.ToList(),
                 Total = collection.Count()
             };
         }
-        
+
+        private static IQueryable<T> Sort<T>(IQueryable<T> collection, string sortOn, string sortOrd)
+        {
+            var param = Expression.Parameter(typeof(T));
+
+            var sortExpression = Expression.Lambda<Func<T, object>>
+                (Expression.Convert(Expression.Property(param, sortOn), typeof(object)), param);
+
+            switch (sortOrd.ToLower())
+            {
+                case "asc":
+                    return collection.AsQueryable<T>().OrderBy<T, object>(sortExpression);
+                default:
+                    return collection.AsQueryable<T>().OrderByDescending<T, object>(sortExpression);
+
+            }
+        }
+
         public class KendoGridResult<T>
         {
             public IEnumerable<T> AggregateResults { get; set; }
